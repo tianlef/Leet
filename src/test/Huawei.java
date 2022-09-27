6db5f637f487a73951d51b74c0883eab8084b2bb
tianlef <1095012807@qq.com>
2022-09-27 14:31:57 +0800

    Test String

--- src/test/Huawei.java
+++ src/test/Huawei.java
@@ -270,6 +270,17 @@ public class Huawei {
     }
 
 
+    public static void reverseString() {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = line.length() - 1; i >= 0 ; i --) {
+            stringBuilder.append(line.charAt(i));
+        }
+        System.out.println(stringBuilder.toString());
+    }
+
+
 
      /**
      * 输入练习
