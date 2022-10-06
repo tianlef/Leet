2f8148117ff3be4b83f3e66b37a9130466d2d975
tianlef <1095012807@qq.com>
2022-10-06 22:53:06 +0800

    一些试题

--- src/main/java/test/Huawei.java
+++ src/main/java/test/Huawei.java
@@ -516,12 +516,50 @@ public class Huawei {
 
     }
 
+
+    /**
+     * 没有重复元素的全排列
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+        List<Integer> output = new ArrayList<Integer>();
+        for (int num : nums) {
+            output.add(num);
+        }
+
+        int n = nums.length;
+        backtrack(n, output, res, 0);
+        return res;
+    }
+
+    public void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
+        // 所有数都填完了
+        if (first == n) {
+            res.add(new ArrayList<Integer>(output));
+        }
+        for (int i = first; i < n; i++) {
+            // 动态维护数组
+            Collections.swap(output, first, i);
+            // 继续递归填下一个数
+            backtrack(n, output, res, first + 1);
+            // 撤销操作
+            Collections.swap(output, first, i);
+        }
+    }
+
+
+
     /**
      * LeetCode 有重复字符串的排列组合
      * @param s
      * @return
      */
     public String[] permutation(String s) {
+        HashSet<String> set = new HashSet<>();
+
         return null;
     }
 
