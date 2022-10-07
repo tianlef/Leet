06163b8f35457ceddc7e5c8e387188e8d3fd0c1f
tianlef <1095012807@qq.com>
2022-10-07 19:11:28 +0800

    有重复字符串的全排列没看懂

--- src/main/java/test/Huawei.java
+++ src/main/java/test/Huawei.java
@@ -552,16 +552,34 @@ public class Huawei {
 
 
 
-    /**
-     * LeetCode 有重复字符串的排列组合
-     * @param s
-     * @return
-     */
-    public String[] permutation(String s) {
-        HashSet<String> set = new HashSet<>();
+    boolean[] vis;
 
-        return null;
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> perm = new ArrayList<>();
+        vis = new boolean[nums.length];
+        Arrays.sort(nums);
+        backtrack(nums,ans,0,perm);
+        return ans;
     }
+
+    public void backtrack(int[] nums, List<List<Integer>> ans, int idx, List<Integer> perm) {
+        if (idx == nums.length) {
+            ans.add(new ArrayList<>(perm));
+            return;
+        }
+        for(int i = 0; i < nums.length; i++) {
+            if (vis[i] || (i > 0 && nums[i] == nums[i-1] && !vis[i-1])) {
+                continue;
+            }
+            perm.add(nums[i]);
+            vis[i] = true;
+            backtrack(nums,ans,idx+1,perm);
+            vis[i] = false;
+            perm.remove(idx);
+        }
+    }
+
 
     /**
      * LeetCode 最长连续递增序列
